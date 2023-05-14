94aa59bc610bfeb50b42067c6733b22e33bb2444
Tamer Ajaj <tamer.ajaj@gmail.com>
2023-05-14 19:28:49 +0200

    Update description

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-#!/usr/bin/env python
-# pylint: disable=unused-argument, wrong-import-position
-# This program is dedicated to the public domain under the CC0 license.
-
 """
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
+Telegram bot to check and change availability of two people.
 """
 
 import logging
